b37c2841d0dc4c0518e21aeec59c869f78782df8
Ankur1903 <ankurzalavadiya11@gmail.com>
2020-12-05 16:41:31 +0530

    Updates

---
+++ precache-manifest.c6b307e65ceb51632084b014d99c4fba.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13c4bb4f3d88416521c8e7d3ab67e819",
+    "url": "/call-webrtc/index.html"
+  },
+  {
+    "revision": "9b2a2f22813337c1296f",
+    "url": "/call-webrtc/static/css/2.f9cc6613.chunk.css"
+  },
+  {
+    "revision": "58b8df87e4491bf000d8",
+    "url": "/call-webrtc/static/css/main.947aa344.chunk.css"
+  },
+  {
+    "revision": "9b2a2f22813337c1296f",
+    "url": "/call-webrtc/static/js/2.9d82a1f1.chunk.js"
+  },
+  {
+    "revision": "ccf3d6b836c43e701840aee472f014f8",
+    "url": "/call-webrtc/static/js/2.9d82a1f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58b8df87e4491bf000d8",
+    "url": "/call-webrtc/static/js/main.9bb0085c.chunk.js"
+  },
+  {
+    "revision": "e8af38311dbd817a9eb5",
+    "url": "/call-webrtc/static/js/runtime-main.dbd2d5dd.js"
+  },
+  {
+    "revision": "0dc760e8ef3d92796b548db312ba4694",
+    "url": "/call-webrtc/static/media/i2.0dc760e8.png"
+  }
+]);
